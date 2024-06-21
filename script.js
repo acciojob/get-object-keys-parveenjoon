4ca31437eee247acb7f6317180765689e5316904
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-06-21 21:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+
 //your JS code here. If required.
+let student = {
+	name: "Jayant Baghel";
+}
+
+Object.prototype.getKeys = function() {
+	return Object.keys(this);
+}
+
+function showKeys() {
+	 console.log(student.getKeys);
+}
